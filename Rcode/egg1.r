d384146e4e93d7295ee2085bcd37b0c24d265fd6
Daliang Ning <ningdaliang@gmail.com>
2015-05-31 03:42:21 -0500

    Debug egg1.r main.r and 1st version output

--- Rcode/egg1.r
+++ Rcode/egg1.r
@@ -1,4 +1,4 @@
-egg1<-function(comi,treat,com.raw,classif,env,prefix,write.output=TRUE)
+egg1<-function(comi,treat,com.raw,classif,level=5,env,prefix,write.output=TRUE)
 {
 # Package "egg" (environment genomic gadgets) initiated by Daliang Ning (ningdaliang@gmail.com)
 # includes all commonly used methods for microbial community data
@@ -8,20 +8,22 @@ library(vegan)
 
 # remove zero-read species
 comm=comi[,colSums(comi)>0]
-
+comm.raw=com.raw[,colSums(com.raw)>0]
 # taxa number
 message("now calculating total number of taxa, sequences and samples. ",date())
 sp.name=colnames(comm)
-sp.num=ncol(comm)
-reads.t=sum(comm)
+sp.num=ncol(comm);sp.raw.num=ncol(comm.raw)
+reads.t=sum(comm);reads.raw.t=sum(comm.raw)
 samp.name=rownames(comm)
-samp.num=nrow(comm)
+samp.num=nrow(comm);samp.raw.num=nrow(comm.raw)
 resamp.read=rowSums(comm)
 if(min(resamp.read)!=max(resamp.read)){message("error! reads after resampling are not equal");break}
-state.1=paste("A total of ",sp.num," taxa were detected from ",reads.t," sequences in ",samp.num," samples after resampled to ",resamp.read[1]," sequences per sample.",sep="")
+state.1=paste("A total of ",sp.num," taxa were detected from ",reads.t," sequences in ",samp.num," samples after resampled to ",resamp.read[1]," sequences per sample. ",
+"A total of ", sp.raw.num, " taxa were detected from ", reads.raw.t, " sequences in ", samp.raw.num, " samples before resampled.", sep="")
 if(write.output){write(state.1,paste("output/",prefix,".01.statement.txt",sep=""))}
 # match
 treat=treat[match(samp.name,rownames(treat)),]
+classif=classif[match(sp.name,rownames(classif)),]
 if(!is.null(nrow(env))){env=env[match(samp.name,rownames(env)),]}
 if(!is.null(nrow(com.raw))){com.raw=com.raw[match(samp.name,rownames(com.raw)),]}
 
@@ -48,7 +50,7 @@ if(write.output){write.csv(dca.sum$spec.scores,paste("output/",prefix,".03.DCAsp
 message("now calculating percentage of each taxon. ",date())
 source("Rcode/per.taxa.r")
 abun=colSums(comm)
-taxa.per=per.taxa(classif,abun=abun,level=2)
+taxa.per=per.taxa(classif,abun=abun,level=level)
 if(write.output){write.csv(taxa.per,paste("output/",prefix,".04.TaxaCom.csv",sep=""))}
 
 # output
--- Rcode/main.r
+++ Rcode/main.r
@@ -1,19 +1,24 @@
 
 ## input ##
-wd="C:/Users/Daliang/Documents/R/GWMC/16S-Merge-0527" # work directory name
-prefix="GW150530M-16SUP" # project name
-read.limit=27000 # min reads per sample
-gene="16S" #ITS or 16S
+# install.packages("vegan")
+wd="C:/Users/Daliang/Dropbox/ToolDevelop/github/egg" # work directory name
+prefix="test" # project name
+read.limit=100 # min reads per sample
+gene="ITS" #ITS or 16S
+its.conf=0.8 # ITS classification confidence threshold
+memory.G=50 # memory limitation
 
 # check following file names if you used other names.
-com.file="otu_table_UP.tabular" # file name of otu table before resample
+com.file="otu_table.txt" # file name of otu table before resample
 treat.file="treat.csv" # file name of treatment information
 samplist.file="samplist.csv" # file for sample name correction
 comr.file="otu_resampled.txt" # file name of otu table after resample
-classif.file="classifier_16S_UP" # classification file
-
+classif.file="classifier" # classification file
 
 ## loading files ##
+library(vegan)
+memory.limit(size=memory.G*1024)
+
 setwd(wd)
 com.raw=read.table(file=paste("input/",com.file,sep=""),header=T,sep="\t",row.names=1);com.raw=t(com.raw)
 if(grepl("tabular",com.file))
@@ -38,14 +43,14 @@ treat=read.table(file=paste("input/",treat.file,sep=""),header=T,sep=",",row.nam
 if(gene=="ITS")
 {
 source("Rcode/tran.ITSrdp.r")
-classif.all=tran.ITSrdp(file=paste("input/",classif.file,".csv",sep=""))
+classif.all=tran.ITSrdp(file=paste("input/",classif.file,".csv",sep=""),conf=its.conf)
 classif=classif.all$taxa
 }else{
 classif=read.table(file=paste("input/",classif.file,".txt",sep=""),header=T,sep="\t",row.names=1)
 }
 
-## 1.1 ## basic diversity analysis
+## 1.1 ## basic diversity analysis, alpha, DCA, taxa overall composition
 source("Rcode/egg1.r")
-com.egg=egg1(comi=com.a,treat=treat,com.raw=com.b,classif=classif,env=NA,prefix)
+com.egg=egg1(comi=com.a,treat=treat,com.raw=com.b,classif=classif,level=5,env=NA,prefix)
 
-## 
+ 
